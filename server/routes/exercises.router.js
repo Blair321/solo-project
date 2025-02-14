cb7c725a213a3852eb873207b175eae2ff80196e
Blair Jilek <blairjilek@gmail.com>
2025-02-13 18:35:59 -0600

    made some changes to the get route

--- server/routes/exercises.router.js
+++ server/routes/exercises.router.js
@@ -3,14 +3,20 @@ const router = express.Router();
 const pool = require('../modules/pool');
 
 // first route for the exercise table
-router.get('/', (req, res) => {
-    console.log('GET /api/exercises');
-    pool.query('SELECT * from "exercises";').then((result) => {
-        res.send(result.rows);
-    }).catch((error) => {
-        console.log('Error GET /api/exercises', error)
-        res.sendStatus(500);
-    });
+router.get( '/', ( req, res )=>{
+    console.log( '/api/exercises GET' );
+    // assemble query
+    const queryString = 'SELECT * FROM exercises';
+    // run pool.query
+    pool.query( queryString ).then( ( results )=>{
+        // return results.rows
+        console.log(results.rows);
+        res.send( results.rows );
+    }).catch( ( err )=>{
+        // handle any errors
+        console.log( err );
+        res.sendStatus( 400 );
+    })
 })
 
 module.exports = router;

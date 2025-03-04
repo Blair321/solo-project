14eae527b8f2895b9631f9438c9d68228f968a60
Blair Jilek <blairjilek@gmail.com>
2025-03-04 15:42:19 -0600

    made chnages to routiens router and took out some commented out code

--- server/routes/routines.router.js
+++ server/routes/routines.router.js
@@ -3,16 +3,7 @@ const router = express.Router();
 const pool = require('../modules/pool');
 const { rejectUnauthenticated, } = require ('../modules/authentication-middleware');
 
- //Get routines seed data
-// router.get('/', rejectUnauthenticated ,(req, res) => {
-//     console.log('/api/routines Get Hit');
-//     const queryString = `SELECT * FROM "workout_routines";`;
-//     pool.query(queryString).then((results)=>
-//     {res.send(results.rows);
-//     }).catch((err)=>{
-//         console.log(err);
-//         res.sendStatus(400);
-//     })
+ 
     
 // });
 router.post('/', async (req, res) => {
